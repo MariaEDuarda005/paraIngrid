3e424e9b2312b28b393bc4a20649e0da8409542e
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-11-28 14:10:44 -0300

    mainpage

--- angular-exemple/src/app/app.component.ts
+++ angular-exemple/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { LoginFormComponent } from "./login-form/login-form.component";
 import { RegisterFormComponent } from "./register-form/register-form.component";
+import { MainPageComponent } from "./main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TodoListComponent, LoginFormComponent, RegisterFormComponent],
+  imports: [MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
